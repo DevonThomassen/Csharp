50e29c87752c9ea513ae6222a9bb5defc028dd1e
DevonThomassen <devon.thomassen@hotmail.com>
2019-04-09 15:03:36 +0200

    Added exercise 1.2

---
+++ opdrachten/opdracht1.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opdracht1._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("HALLOOOO");
+            Console.ReadKey();
+        }
+
+    }
+}
